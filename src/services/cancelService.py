593856b12b58c639c31acba3615517bb6b61ad2f
GaurangPohankar <pohankargaurang@gmail.com>
2023-08-30 15:32:17 +0530

    Update: removed unnecessory code from cancel course service

--- src/services/cancelService.py
+++ src/services/cancelService.py
@@ -6,14 +6,9 @@ class CancelService:
     def cancel_course(self, tbl_register, parts):
         course_registration_id = parts[1]
 
-        if self.is_registration_cancelled(tbl_register, course_registration_id):
-            updated_register = self.update_registration_status(tbl_register, course_registration_id, 'CANCELLED')
+        if utility.cal_is_registration_cancelled(tbl_register, course_registration_id):
+            updated_register = utility.cal_update_status(tbl_register, course_registration_id, 'CANCELLED')
             return f"{course_registration_id} {STATUS_CANCEL_ACCEPTED}"
         else:
             return f"{course_registration_id} {STATUS_CANCEL_REJECTED}"
 
-    def is_registration_cancelled(self, tbl_register, course_registration_id):
-        return utility.cal_is_registration_cancelled(tbl_register, course_registration_id)
-
-    def update_registration_status(self, tbl_register, course_registration_id, status):
-        return utility.cal_update_status(tbl_register, course_registration_id, status)
